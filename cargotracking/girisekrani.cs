cd5279ab064d9006b3561b2f24d117bce809fa3a
KEREM FARUK <2306001065@student.beykent.edu.tr>
2025-06-10 01:09:23 +0300

    tasarım

---
+++ cargotracking/girisekrani.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cargotracking
+{
+    public partial class girisekrani : Form
+    {
+        public girisekrani()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Panel3_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
